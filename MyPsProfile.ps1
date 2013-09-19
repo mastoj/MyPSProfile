cada6bd03ec9080089c5199599b70b5762979234
Tomas Jansson <tomas.jansson@gmail.com>
2013-09-19 21:48:41 +0200

    vs13

--- MyPsProfile.ps1
+++ MyPsProfile.ps1
@@ -1,3 +1,4 @@
 set-alias vs12 "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
 set-alias msbuild C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 set-alias subl "C:\Program Files\Sublime Text 2\sublime_text.exe"
+set-alias vs13 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
